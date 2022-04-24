88a6d4193c324a40fc7002e2c2bb40e098ab9c11
Sanntalol <87233478+Sanntalol@users.noreply.github.com>
2022-04-24 10:39:22 +0700

    Add files via upload

---
+++ Fortune Teller Activity/story.js
@@ -0,0 +1,21 @@
+let greetings = "Welcome to my hideout"
+let name = "Bane"
+let title = "Bone breaker"
+let world = "Underworld"
+
+let age = 600
+let year = 9000
+let century = 90
+
+let weapon = ['Bone breaker suit', 'Machine gun']
+
+let favweapon = true
+
+console.log(greetings + "My name is"+ name + ".")
+console.log("I am known as the" + title)
+console.log("As the ruler of" + world + "I need to control everybody")
+console.log("In my world, everybody obeys me")
+console.log("People call me ancient because I am" + age + "Years old")
+console.log("In your world it is 2022 but in my world it is" + year)
+console.log("Whenever i want to punish some bandits I like to use my" + weapon)
+console.log("If you want to face me I will welcome you everytime")
